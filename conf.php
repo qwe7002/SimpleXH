9707910e9e609b33a41eaabcc2705dbf38e12a68
qwe7002 <381511791@qq.com>
2015-01-26 14:51:46 +0800

    修改左栏连接输出方式

--- conf.php
+++ conf.php
@@ -1,18 +1,21 @@
-<?php
+﻿<?php
 define('NAME', 'qwe7002');//博主名
 define('MAIL', '381511791@qq.com');//博主邮箱
-define('ABOUTME', 'index.php/start-page.html');//关于我页面地址
-define('LINK', 'index.php/link.html');//友情连接
-define('CATEGORYLIFEURL', 'index.php/category/life');//科技分类页面
-define('CATEGORYTECHNOLOGYURL', 'index.php/category/Technology');//科技分类页面
 define('GITHUB', 'qwe7002');//Github用户名
 define('WEIBO','<iframe width="auto" height="800px" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=550&fansRow=2&ptype=1&speed=0&skin=2&isTitle=1&noborder=0&isWeibo=1&isFans=0&uid=1793923850&verifier=d5a9b3c2&dpc=1"></iframe>');//微博名
 define('TWITTER','qwe7002');
 define('STATISTICS','');//统计
+$mainlinks=array("ABOUT ME"=>"index.php/start-page.html",
+                 "友情链接"=>"index.php/start-page.html");
+$categorylinks=array("生活"=>"index.php/category/life",
+					 "科技"=>"index.php/category/Technology",
+					 "宅物"=>"index.php/category/acger");
 $friendlinks=array("starriv | 关注安全与研发"=>"https://starriv.com/",
 				   "DELUXGHOST"=>"http://deluxghost.me/",
 				   "Not ready yet(Cry)"=>"http://infinnie.github.io/",
 				   "faceair的博客"=>"http://lucy.faceair.me/",
-				   "Drocula @ 鱼美美 |"=>"http://www.iblo.info/");
+				   "Drocula @ 鱼美美 |"=>"http://www.iblo.info/",
+				   "Home &middot; orzFly"=>"https://orzfly.com/",
+				   );
 //友情连接
 ?>
--- sidebar-left.php
+++ sidebar-left.php
@@ -24,21 +24,25 @@ include("conf.php");
     <div id= "nav" class="am-offcanvas-content">
     <div id="list">
    <a href="<?php $this->options->siteUrl(); ?>"> <span id="ico" class="am-icon-home"></span>  &nbsp;&nbsp;&nbsp;主页</a>
-      <a href="<?php
-      $this->options->siteUrl();
-      echo ABOUTME;
-      ?>"><span id="ico" class="am-icon-angle-right"></span>  &nbsp;&nbsp;&nbsp;About ME</a>
-      <a href="<?php
-      $this->options->siteUrl();
-      echo LINK;
-      ?>"><span id="ico" class="am-icon-angle-right"></span>  &nbsp;&nbsp;&nbsp;友情连接</a>
- 
-        
-        <div class="am-dropdown am-dropdown-up" data-am-dropdown>
+   <?php 
+  foreach($mainlinks as $x=>$x_value) {
+  echo '<a href="';
+  echo $this->options->siteUrl();
+  echo $x_value;
+  echo '"><span id="ico" class="am-icon-angle-right"></span>  &nbsp;&nbsp;&nbsp;'.$x;
+  echo "</a>";
+}?>
+  <div class="am-dropdown am-dropdown-up" data-am-dropdown>
   <button class="am-btn am-btn-danger am-dropdown-toggle shangla" data-am-dropdown-toggle><span id="ico" class="am-icon-angle-right"></span>&nbsp;&nbsp;分类&nbsp;&nbsp;<span class="am-icon-caret-up"></span></button> <ul class="am-dropdown-content shangla">
-  
-            <li class="am-active"><a href="<?php $this->options->siteUrl(); echo CATEGORYLIFEURL;?>">生活</a></li>
-            <li class="am-active"><a href="<?php $this->options->siteUrl(); echo CATEGORYTECHNOLOGYURL;?>">技术</a></li>
+  <?php 
+  foreach($categorylinks as $x=>$x_value) {
+  echo '<li class="am-active"><a href="';
+  echo $this->options->siteUrl();
+  echo $x_value;
+  echo '">'.$x;
+  echo "</a></li>";
+}
+  ?>
   </ul>
 		</div>
       </div>
